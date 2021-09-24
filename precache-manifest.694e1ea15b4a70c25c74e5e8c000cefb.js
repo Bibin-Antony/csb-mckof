73a37fbd984257138a22a8562402fdb84066cc73
Code Sandbox <hello@codesandbox.io>
2021-09-24 10:11:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.694e1ea15b4a70c25c74e5e8c000cefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52240c0688d264b76e5f8104ff85c5fe",
+    "url": "https://bibin-antony.github.io/csb-mckof/index.html"
+  },
+  {
+    "revision": "957f875af248f2073ead",
+    "url": "https://bibin-antony.github.io/csb-mckof/static/css/main.c41ee3b4.chunk.css"
+  },
+  {
+    "revision": "e945b3899165a8bce9c7",
+    "url": "https://bibin-antony.github.io/csb-mckof/static/js/2.0e8e227e.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://bibin-antony.github.io/csb-mckof/static/js/2.0e8e227e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957f875af248f2073ead",
+    "url": "https://bibin-antony.github.io/csb-mckof/static/js/main.32b6a2c6.chunk.js"
+  },
+  {
+    "revision": "b27191fb7abfbbbcc0e0",
+    "url": "https://bibin-antony.github.io/csb-mckof/static/js/runtime-main.83709181.js"
+  }
+]);
